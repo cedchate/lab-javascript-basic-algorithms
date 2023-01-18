5a8e5a3aacf56700b7ce5775f4a5f8404827d33c
cedchate <cedricchatellier95@gmail.com>
2023-01-18 15:38:51 +0100

    add iteration 3

--- index.js
+++ index.js
@@ -13,3 +13,43 @@ else if (hacker1.length < hacker2.length)
 else
     console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
 // Iteration 3: Loops
+
+let driverCapital = "";
+
+for(i = 0 ; i < hacker1.length; i++){
+	
+	driverCapital += hacker1[i].toUpperCase();
+	
+	if(i != hacker1.length-1) { 
+		driverCapital += " ";
+	}
+	
+}
+
+let navigatorReverse= "" ;
+
+for (i = hacker2.length-1; i >= 0; i--){
+	navigatorReverse += hacker2[i];
+}
+
+console.log(driverCapital);
+console.log(navigatorReverse);
+
+if (hacker1 < hacker2){
+	console.log("The driver's name goes first.");
+	
+}else if (hacker1 < hacker2){
+	console.log("Yo, the navigator goes first definitely.");
+	
+}else 
+	console.log("What?! You both have the same name?");
+
+
+
+
+
+
+
+
+
+
